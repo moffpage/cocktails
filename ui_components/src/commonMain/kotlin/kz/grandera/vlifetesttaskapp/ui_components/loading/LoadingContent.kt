c2316919dc5fc1a67846090f6078699e9ae7c949
arturmavl <moffpage@inbox.ru>
2024-10-26 15:52:08 +0500

    Updated LoadingContent with new 2.0 Compottie API

--- ui_components/src/commonMain/kotlin/kz/grandera/vlifetesttaskapp/ui_components/loading/LoadingContent.kt
+++ ui_components/src/commonMain/kotlin/kz/grandera/vlifetesttaskapp/ui_components/loading/LoadingContent.kt
@@ -1,24 +1,19 @@
 package kz.grandera.vlifetesttaskapp.ui_components.loading
 
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
-import io.github.alexzhirkevich.compottie.LottieAnimation
+import io.github.alexzhirkevich.compottie.Compottie
 import io.github.alexzhirkevich.compottie.LottieCompositionSpec
-import io.github.alexzhirkevich.compottie.animateLottieCompositionAsState
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
 import io.github.alexzhirkevich.compottie.rememberLottieComposition
 
 import kz.grandera.vlifetesttaskapp.ui_components.Res
@@ -32,36 +27,27 @@ public fun LoadingContent(
     enableInput: Boolean = false,
     backgroundColor: Color = MaterialTheme.colors.background
 ) {
-    var jsonString by remember { mutableStateOf(value = "") }
-
-    val composition by rememberLottieComposition(
-        spec = LottieCompositionSpec.JsonString(
-            jsonString = jsonString
-        )
-    )
-
-    val progress by animateLottieCompositionAsState(
-        composition = composition,
-        iterations = Int.MAX_VALUE
-    )
-
-    LaunchedEffect(Unit) {
-        jsonString = Res.readBytes("files/cocktail_animation.json")
-            .decodeToString()
-    }
-
     Box(
         modifier = modifier
             .background(color = backgroundColor.copy(alpha = alpha))
-            .fillMaxSize()
             .disableInput(allow = enableInput)
+            .fillMaxSize()
     ) {
-        LottieAnimation(
-            modifier = Modifier
-                .align(alignment = Alignment.Center)
-                .matchParentSize(),
-            progress = { progress },
-            composition = composition
+        Image(
+            modifier = Modifier.matchParentSize(),
+            painter = rememberLottiePainter(
+                composition = rememberLottieComposition(
+                    spec = {
+                        LottieCompositionSpec.JsonString(
+                            jsonString = Res.readBytes(
+                                path = "files/cocktail_animation.json"
+                            ).decodeToString()
+                        )
+                    }
+                ).value,
+                iterations = Compottie.IterateForever
+            ),
+            contentDescription = null
         )
     }
 }
